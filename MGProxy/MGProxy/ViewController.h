0033af40c47b8d2c254b58c241518129449fc3af
guoqianling <guoqianling@babytree-inc.com>
2017-03-15 09:52:53 +0800

    Init commit
    add files

---
+++ MGProxy/MGProxy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MGProxy
+//
+//  Created by guoqianling on 2017/3/15.
+//  Copyright © 2017年 MarvesG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MGProxy/MGProxy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MGProxy
+//
+//  Created by guoqianling on 2017/3/15.
+//  Copyright © 2017年 MarvesG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
